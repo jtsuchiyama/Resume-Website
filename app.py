b139174a69ebcdd000cdcc2f7367562f44e185f1
jtsuchiyama <jtsuchiyama@scu.edu>
2021-01-17 18:59:08 -1000

    Adding webpages

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route('/art/')
+def art():
+    return render_template("art.html")
+
+@app.route("/music/")
+def music():
+    return render_template("music.html")
+
+@app.route('/projects/')
+def projects():
+    return render_template("projects.html")
+
